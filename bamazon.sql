9076b71884004860435a0c5ec3d54071107dabea
Garrett Moore <garrettmarkmoore@gmail.com>
2017-08-27 22:14:03 -0700

    First commit. Added .gitignore and bamazonCustomer.js

---
+++ bamazon.sql
@@ -0,0 +1,42 @@
+USE bamazon_DB;
+
+CREATE TABLE products (
+	item_id INTEGER NOT NULL AUTO_INCREMENT,
+    product_name VARCHAR(100) NULL,
+    department_name VARCHAR(100) NULL,
+    price DECIMAL(10,2) NULL,
+    stock_quantity INTEGER(10) NULL,
+    PRIMARY KEY (item_id)
+);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Legend of Zelda: Breath of the Wild", "Video Games", 59.99, 3000);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Bose QuietComfort 35", "Headphones ", 349.00, 4000);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Apple iPad Pro", "Tablet", 839.99, 1500);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("ASUS Portable Monitor", "Monitors",154.64, 2500);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Amazon Fire Stick", "Networking",39.99, 5000);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Samsung Chromebook Plus", "Tablets", 425.99, 2000);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("LG 5K Monitor", "Monitors", 949.99, 3500);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Adobe Photoshop Elements", "Software", 59.99, 5000);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Nintendo Switch", "Video Games", 376.50, 200);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Splatoon 2", "Video Games", 58.95, 1000);
+
+
---
+++ bamazonCustomer.js
@@ -0,0 +1,16 @@
+//Installing npm packages
+var mysql = require("mysql");
+var inquirer = require('inquirer');
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "",
+    database: "bamazon_DB"
+  });
